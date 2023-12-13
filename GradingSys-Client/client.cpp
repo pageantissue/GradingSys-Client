6856dd0913720385b944d28013f0660b66909c06
Jingfeng Deng <jingfeng_deng@outlook.com>
2023-12-13 16:02:05 +0800

    Changes

--- GradingSys-Client/client.cpp
+++ GradingSys-Client/client.cpp
@@ -2,15 +2,12 @@
 
 int main()
 {
-    int client_sock = socket(AF_INET, SOCK_STREAM, 0);//建立客户端socket
-
-    // 设置服务器地址信息
+    int client_sock = socket(AF_INET, SOCK_STREAM, 0);
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(SERVER_PORT);  // 服务器监听端口
-    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");  // 服务器 IP 地址
+    serverAddress.sin_port = htons(SERVER_PORT);
+    serverAddress.sin_addr.s_addr = inet_addr("10.37.240.154");
 
-    // 连接到服务器
     if (connect(client_sock, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
         perror("Connection failed");
         close(client_sock);
@@ -21,7 +18,6 @@ int main()
     while (1)
     {
         memset(recvbuf, '\0', sizeof(recvbuf));
-        // 接收服务器端发送的信息
         ssize_t len = recv(client_sock, recvbuf, sizeof(recvbuf), 0);
         if (len <= 0) {
             printf("Nothing received or failures ocurred!\n");
@@ -36,18 +32,15 @@ int main()
 
         if (strchr(recvbuf, '\n') != NULL)
         {
-            // 如果包含换行符，则认为是无需回复的信息，不需要用户输入，跳过后续逻辑
             continue;
         }
 
         fgets(sendbuf, sizeof(sendbuf), stdin);
-        // 输入exit退出系统
         if (strcmp(sendbuf, "exit\n") == 0)
             break;
         sendbuf[strlen(sendbuf) - 1] = '\0';
-        // 发送用户命令到服务器端
         send(client_sock, sendbuf, strlen(sendbuf), 0);
     }
-    close(client_sock);//关闭客户端socket
+    close(client_sock);
     return 0;
 }
--- GradingSys-Client/client.h
+++ GradingSys-Client/client.h
@@ -10,39 +10,39 @@
 #include<unistd.h>
 #include<cerrno>
 
-#define SERVER_PORT 6591 //服务端口
+#define SERVER_PORT 6591
 #define BUF_SIZE 1024
 using namespace std;
 
 void help() {
-	cout.setf(ios::left); // 设置对齐方式为left
-	cout.width(30); //设置宽度，不足用空格填充 
+	cout.setf(ios::left);
+	cout.width(30);
 	//cout << setiosflags(ios::left);
-	cout << "ls" << "Display the current directory listing" << endl;	//列出当前目录清单
+	cout << "ls" << "Display the current directory listing" << endl;	//锟叫筹拷锟斤拷前目录锟藉单
 	cout.width(30);
-	cout << "cd" << "Enter the specific directory " << endl;		//前往指定目录
+	cout << "cd" << "Enter the specific directory " << endl;		//前锟斤拷指锟斤拷目录
 	cout.width(30);
-	cout << "mkdir" << "Create directory" << endl;					//创建目录
+	cout << "mkdir" << "Create directory" << endl;					//锟斤拷锟斤拷目录
 	cout.width(30);
-	cout << "rm" << "Delete the file or directory" << endl;			//删除文件和目录 
+	cout << "rm" << "Delete the file or directory" << endl;			//删锟斤拷锟侥硷拷锟斤拷目录 
 	cout.width(30);
-	cout << "touch" << "Create new file" << endl;				//创建新文件
+	cout << "touch" << "Create new file" << endl;				//锟斤拷锟斤拷锟斤拷锟侥硷拷
 	cout.width(30);
-	cout << "read" << "Read the content of file" << endl;		//读文件
+	cout << "read" << "Read the content of file" << endl;		//锟斤拷锟侥硷拷
 	cout.width(30);
-	cout << "write" << "Write the file" << endl;			//写文件
+	cout << "write" << "Write the file" << endl;			//写锟侥硷拷
 	cout.width(30);
-	cout << "chmod" << "Modify the access right" << endl;		//修改文件权限
+	cout << "chmod" << "Modify the access right" << endl;		//锟睫革拷锟侥硷拷权锟斤拷
 	cout.width(30);
-	cout << "adduser" << "Add user" << endl;		//新增用户
+	cout << "adduser" << "Add user" << endl;		//锟斤拷锟斤拷锟矫伙拷
 	cout.width(30);
-	cout << "deluser" << "Delete user" << endl;		//删除用户
+	cout << "deluser" << "Delete user" << endl;		//删锟斤拷锟矫伙拷
 	cout.width(30);
-	cout << "addusergrp" << "Add user group" << endl;		//新增用户组
+	cout << "addusergrp" << "Add user group" << endl;		//锟斤拷锟斤拷锟矫伙拷锟斤拷
 	cout.width(30);
-	cout << "delusergrp" << "Delete user group" << endl;		//删除用户组
+	cout << "delusergrp" << "Delete user group" << endl;		//删锟斤拷锟矫伙拷锟斤拷
 	cout.width(30);
-	cout << "snapshot" << "Back up the system" << endl;			//备份系统
+	cout << "snapshot" << "Back up the system" << endl;			//锟斤拷锟斤拷系统
 	cout.width(30);
 	cout << "format" << "Recover the system" << endl;
 	cout.width(30);
