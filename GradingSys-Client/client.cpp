86791e58d37cdb1aaf4718ab5933af276dc9e696
Yoshioen <jingfeng_deng@outlook.com>
2023-12-13 16:32:04 +0800

    11

--- GradingSys-Client/client.cpp
+++ GradingSys-Client/client.cpp
@@ -29,16 +29,18 @@ int main()
             continue;
         }
         printf(recvbuf);
-
+        fflush(stdout);
+        fflush(stdin);
         if (strchr(recvbuf, '\n') != NULL)
         {
             continue;
         }
-
+        printf("准备接收用户输入\n");
         fgets(sendbuf, sizeof(sendbuf), stdin);
+        printf(sendbuf);
         if (strcmp(sendbuf, "exit\n") == 0)
             break;
-        sendbuf[strlen(sendbuf) - 1] = '\0';
+        //sendbuf[strlen(sendbuf) - 1] = '\0';
         send(client_sock, sendbuf, strlen(sendbuf), 0);
     }
     close(client_sock);
