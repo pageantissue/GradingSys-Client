08e2833841768847914fc18c33432a16fc69b907
Yoshioen <jingfeng_deng@outlook.com>
2023-12-15 05:48:28 +0800

    更新测试

--- GradingSys-Client/client.h
+++ GradingSys-Client/client.h
@@ -15,43 +15,60 @@
 using namespace std;
 
 void help1() { //学生的命令提示
-
+	cout.setf(ios::left); //设置对齐方式为left 
+	cout.width(30); //设置宽度，不足用空格填充 
+	cout << "checkHw [courseName] [taskName]" << "View the content of the corresponding assignment " << endl;  //查看作业内容
+	cout.width(30);
+	cout << "submit [courseName] [taskName]" << "Submit an assignment" << endl;  //提交作业
+	cout.width(30);
+	cout << "checkScore [courseName] [taskName]" << "View the score of the corresponding assignment " << endl;  //查看作业分数
 }
 
 void help2() { //教师的命令提示
-
+	cout.setf(ios::left); //设置对齐方式为left 
+	cout.width(30); //设置宽度，不足用空格填充 
+	cout << "publish [courseName] [taskName]" << "Publish a task" << endl;  //布置作业
+	cout.width(30);
+	cout << "judge [courseName] [taskName]" << "Judging homework" << endl;  //给作业打分
 }
-void help() {
+
+void help() { // root的命令提示
 	cout.setf(ios::left);
 	cout.width(30);
-	//cout << setiosflags(ios::left);
-	cout << "ls" << "Display the current directory listing" << endl;	//�г���ǰĿ¼�嵥
+	cout << "ls [-l]" << "Display the current directory listing" << endl;
 	cout.width(30);
-	cout << "cd" << "Enter the specific directory " << endl;		//ǰ��ָ��Ŀ¼
+	cout << "cd [path]" << "Enter the specific directory " << endl;
 	cout.width(30);
-	cout << "mkdir" << "Create directory" << endl;					//����Ŀ¼
+	cout << "gotoRoot " << "Return to the root directory " << endl;
 	cout.width(30);
-	cout << "rm" << "Delete the file or directory" << endl;			//ɾ���ļ���Ŀ¼ 
+	cout << "mkdir [path]" << "Create directory" << endl;
 	cout.width(30);
-	cout << "touch" << "Create new file" << endl;				//�������ļ�
+	cout << "rm [-rf dir_path] | [-f file_path]" << "Delete directory or file" << endl;
 	cout.width(30);
-	cout << "read" << "Read the content of file" << endl;		//���ļ�
+	cout << "touch" << "Create a blank file" << endl;
 	cout.width(30);
-	cout << "write" << "Write the file" << endl;			//д�ļ�
+	cout << "echo \"content\" > [path] | \"content\" >> [path]" << "Write or add a file" << endl;
 	cout.width(30);
-	cout << "chmod" << "Modify the access right" << endl;		//�޸��ļ�Ȩ��
+	cout << "cat [file_path]" << "View the file content" << endl;
 	cout.width(30);
-	cout << "adduser" << "Add user" << endl;		//�����û�
+	cout << "chmod [ugoa]*[-+=]*[rwx] | [0-7]+" << "Modify the access right" << endl;
 	cout.width(30);
-	cout << "deluser" << "Delete user" << endl;		//ɾ���û�
+	cout << "chown [user:group] [path]" << "Modify user and group in file" << endl;
 	cout.width(30);
-	cout << "addusergrp" << "Add user group" << endl;		//�����û���
+
+	cout << "useradd [-g group] [-m] [user]" << "Add user" << endl;
 	cout.width(30);
-	cout << "delusergrp" << "Delete user group" << endl;		//ɾ���û���
+	cout << "userdel [user]" << "Delete user" << endl;
 	cout.width(30);
-	cout << "snapshot" << "Back up the system" << endl;			//����ϵͳ
+	cout << "groupadd [group]" << "Add group" << endl;
 	cout.width(30);
-	cout << "format" << "Recover the system" << endl;
+	cout << "groupdel [group]" << "Delete group" << endl;
+	cout.width(30);
+	cout << "passwd [user]" << "Modify the password" << endl;
+	help1(); help2();
+	cout.width(30);
+	cout << "logout" << "Logout the account" << endl;
 	cout.width(30);
 	cout << "exit" << "Exit the system" << endl;
+	cout.width(30);
 }
